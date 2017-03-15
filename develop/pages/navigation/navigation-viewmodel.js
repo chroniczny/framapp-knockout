654957291c808c97ad829b3e6d2fb8b900f75f57
PGS-SOFT\mgierczak <mgierczak@pgs-soft.com>
2017-03-15 10:07:18 +0100

    Added new files

---
+++ develop/pages/navigation/navigation-viewmodel.js
@@ -0,0 +1,14 @@
+var NavViewModel = function () {
+    var self = this;
+
+    self.categories = [];
+    self.categoriesList = ko.observableArray(this.categories);
+
+    self.getProductsCategory = function () {
+        $.getJSON('https://project-5613440220430148247.firebaseio.com/api/v1/categories.json', function (json) {
+            $.map(json, function (elem) { self.categoriesList.push(elem); });
+        });
+    };
+
+    self.getProductsCategory();
+};
--- develop/scripts/viewmodels.js
+++ develop/scripts/viewmodels.js
@@ -1,12 +1,8 @@
 ﻿var MainViewModel = function () {
 
-    var NavViewModel = function () {
+    requirejs(["navViewModel"], function (myapp) {
 
-    };
-
-    //requirejs(["navViewModel"], function (myapp) {
-
-    //});
+    });
 
 
     var ProductsViewModel = function () {
@@ -21,28 +17,6 @@
     var DetailProductViewModel = function () {
 
     };
-
-    
-
-    //this.tickets = [{ name: "Economy", price: 199.05 }, { name: "Business", price: 299.00 }, { name: "First Class", price: 399.50 }];
-    //this.chosenTicket = ko.observable();
-    //this.resetTicket = function () { this.chosenTicket(null) };
-
-
-    
-    this.getProductsCategory = function () {
-        $.getJSON('https://project-5613440220430148247.firebaseio.com/api/v1/categories.json', function (json) {
-            return $.map(json, function (elem) { return elem; });
-        });
-    };
-    
-    //this.categories = [{
-    //    description: 'ala ma kota',
-    //    title: 'moo'
-    //}];
-
-    this.categories = this.getProductsCategory();
-
 };
 
 ko.applyBindings(new MainViewModel());
