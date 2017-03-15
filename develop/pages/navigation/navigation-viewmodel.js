36c1afba199e7e338a9387f99316913e62cbb07d
PGS-SOFT\mgierczak <mgierczak@pgs-soft.com>
2017-03-15 13:55:26 +0100

    Mass changes in project files

--- develop/pages/navigation/navigation-viewmodel.js
+++ develop/pages/navigation/navigation-viewmodel.js
@@ -2,7 +2,7 @@ var NavViewModel = function () {
     var self = this;
 
     self.categories = [];
-    self.categoriesList = ko.observableArray(this.categories);
+    self.categoriesList = ko.observableArray(self.categories);
 
     self.getProductsCategory = function () {
         $.getJSON('https://project-5613440220430148247.firebaseio.com/api/v1/categories.json', function (json) {
