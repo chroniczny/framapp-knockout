b3cd4322fd3ac126285c302c77bd938cbda7f203
PGS-SOFT\mgierczak <mgierczak@pgs-soft.com>
2017-03-16 09:11:46 +0100

    added

--- develop/pages/navigation/navigation-viewmodel.js
+++ develop/pages/navigation/navigation-viewmodel.js
@@ -2,7 +2,7 @@ var NavViewModel = function () {
     var self = this;
     self.categories = [];
     self.categoriesList = ko.observableArray(self.categories);
-    self.categoriesJson = '../data/generated-data.json'; //'https://project-5613440220430148247.firebaseio.com/api/v1/categories.json';
+    self.categoriesJson = 'https://frammapp-knockout.firebaseio.com/.json'; //'https://project-5613440220430148247.firebaseio.com/api/v1/categories.json';
     var dm = new DataModel($.getJSON, $.map, self.categoriesJson, self.categoriesList);
     dm.getProductsCategory();
 };
