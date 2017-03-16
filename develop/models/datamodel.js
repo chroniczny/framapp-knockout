5586a986424db824f5dd6b9f803d7e5f0a0f59dc
PGS-SOFT\mgierczak <mgierczak@pgs-soft.com>
2017-03-16 13:57:17 +0100

    aaa

--- develop/models/datamodel.js
+++ develop/models/datamodel.js
@@ -1,7 +1,10 @@
 var DataModel = function (getJSON, map, categoriesJson, categories) {   // used dependency injection (var dm = new DataModel($.getJSON, $.map, n.categoriesJson);)
     var getProductsCategory = function () {
         getJSON(categoriesJson, function (json) {
-            map(json, function (elem) { categories.push(elem); console.log('aa'); console.log(categories) }); // not pure function
+            map(json, function (elem) { 
+                categories.push(elem); 
+                //console.log(categories());
+            }); // not pure function
         });
     };
 
