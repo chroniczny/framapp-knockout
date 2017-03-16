5b14fa25489fc05d58eba0d6764160d89bc6fb85
jkwiatkowski <jkwiatkowski@pgs-soft.com>
2017-03-16 15:13:17 +0100

    PFAP-1_SHOP_-_Main_Layout: some changes in datamodel.js

--- develop/models/datamodel.js
+++ develop/models/datamodel.js
@@ -1,10 +1,9 @@
 var DataModel = function (getJSON, map, categoriesJson, categories) {   // used dependency injection (var dm = new DataModel($.getJSON, $.map, n.categoriesJson);)
     var getProductsCategory = function () {
         getJSON(categoriesJson, function (json) {
-            var firstJSON = json['categories'];
-            map(firstJSON, function (elem) {
-                // console.log(elem);
-                categories.push(Object.values(elem));
+            var firstJSON = json.categories; // in json.categories we have collection
+            map(firstJSON, function (elem) { // elem is a single category
+                categories.push(elem.title); // we need a value of it's only param (title)
             }); // not pure function (two objects in collection - function: 2 times)
             console.log(categories());
         });
