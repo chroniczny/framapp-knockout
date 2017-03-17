6ffc20c09a8c7d0370b6b256d074f901f7989465
PGS-SOFT\mgierczak <mgierczak@pgs-soft.com>
2017-03-17 15:50:49 +0100

    Modified

--- develop/pages/navigation/navigation-viewmodel.js
+++ develop/pages/navigation/navigation-viewmodel.js
@@ -5,4 +5,15 @@ var NavViewModel = function () {
     self.categoriesJson = 'https://frammapp-knockout.firebaseio.com/.json'; //'https://project-5613440220430148247.firebaseio.com/api/v1/categories.json';
     var dm = new DataModel($.getJSON, $.map);
     dm.getCategories(self.categoriesJson, self.categoriesList);
+
+    // choosing items
+
+    self.isPromoted = ko.observable();
+
+    self.ourCategory = ko.observable();
+    self.getCategory = function(elem) {
+        self.ourCategory(elem);
+        console.log('z funkcji getCategory: ' + elem);
+    }
+
 };
--- develop/pages/products/products-viewmodel.js
+++ develop/pages/products/products-viewmodel.js
@@ -1,8 +1,19 @@
-var ProductsViewModel = function () {
+var ProductsViewModel = function (navObj) {
     var self = this;
     self.products = [];
     self.productsList = ko.observableArray(self.products);
     self.productsJson = 'https://frammapp-knockout.firebaseio.com/.json'; //'https://project-5613440220430148247.firebaseio.com/api/v1/categories.json';
     var dm = new DataModel($.getJSON, $.map);
     dm.getProducts(self.productsJson, self.productsList, 'promoted', true);
+
+    //
+
+    // self.showProducts = ko.computed(function() {
+    //     if (navObj.ourCategory() === 'Phones') {
+    //         return true;
+    //         console.log(navObj.ourCategory);
+    //     } else {
+    //         return false;
+    //     }
+    // });
 };
